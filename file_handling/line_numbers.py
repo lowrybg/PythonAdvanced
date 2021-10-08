25ded7442335ce0054873bc12df42dba6291c2d0
Taner <lowrybg@users.noreply.github.com>
2021-10-08 21:04:28 +0300

    File handling

---
+++ file_handling/line_numbers.py
@@ -0,0 +1,20 @@
+
+with open("text.txt", "r") as file:
+    lines = file.readlines()
+    counter = 1
+    letters = 0
+    punct = 0
+    puncs = "[,.!?-\"\']"
+    for l in lines:
+        for ch in l:
+            if ch.isalpha():
+                letters += 1
+            elif ch in puncs:
+                punct += 1
+        output = open("output.txt", "a")
+        output.write(f'Line {counter}: {l[:-1]} ({letters}) ({punct})\n')
+        output.close()
+
+        letters = 0
+        punct = 0
+        counter += 1
